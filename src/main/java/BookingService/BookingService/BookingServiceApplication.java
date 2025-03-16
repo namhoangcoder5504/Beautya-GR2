9b8a4ff4595ea5cf361f96de8ae33d864cd4dcee
Nam <namhoang0505pro@gmail.com>
2025-03-16 12:25:29 +0700

    update time

--- src/main/java/BookingService/BookingService/BookingServiceApplication.java
+++ src/main/java/BookingService/BookingService/BookingServiceApplication.java
@@ -1,17 +1,19 @@
 package BookingService.BookingService;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class BookingServiceApplication {
     public static void main(String[] args) {
+        // Đặt múi giờ mặc định cho JVM (dự phòng)
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+//        System.out.println("Default TimeZone set to: " + TimeZone.getDefault().getID());
+
         SpringApplication.run(BookingServiceApplication.class, args);
     }
 }
-
-
